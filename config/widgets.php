8104220fb423790b7a3da11d8a33e9ae93e11c37
ryun <ryan@letsgolearn.com>
2022-08-03 10:08:57 -0700

    updated config for widgets
    Signed-off-by: ryun <ryan@letsgolearn.com>

--- config/widgets.php
+++ config/widgets.php
@@ -1,9 +1,37 @@
 <?php
-// config for Humweb/Thingamabob
-return [
-    'route' => '/api/widgets',
-    'middleware' => [],
 
-    'assets_path' => resource_path('js/widgets/'),
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base route
+    |--------------------------------------------------------------------------
+    |
+    | This is the URI path where the widget data will be accessible from. Feel free
+    | to change this path to anything you like.
+    |
+    */
+    'route'       => '/api/widgets',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Middleware for internal routing
+    |--------------------------------------------------------------------------
+    |
+    | You may add middlewares to restrict the internal routess
+    |
+    */
+    'middleware'  => [],
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Assets path
+    |--------------------------------------------------------------------------
+    |
+    | This path is where you want to publish widgets from other packages.
+    |
+    */
+    'assets_path' => resource_path('js/Widgets/'),
 
 ];
